1f70f806ae815ed098871ceb5794ceaa4a63537b
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-17 14:52:51 +0000

    added 01-variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14425
+let accountEmail = "wasif@google.com"
+var accountPassword = "12345"
+accountCity = 'jaipur'
+let accountState 
+
+// accountId = 1212
+
+accountEmail = "wf@google.com"
+accountPassword = "12345"
+accountCity = "Bangluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log(typeof accountCity)
