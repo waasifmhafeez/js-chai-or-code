981df9d0b0bfa8e784586a53dd7a5d2a50504ad8
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-17 14:53:31 +0000

    added 02_dataTypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" //treats all js code as newer version of JS , nowadays standard goes so high that automatically everything will runs at strict mode
+
+alert(3 + 3)  //we're using node js, not browser
+
+console.log(3
+    +
+    3)  // code readability should be high
+
+console.log("wasif");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true //true or false
+
+
+// number => 2 to power 53
+// bigint
+// string => should be used with double quotes ""
+// boolean => true/ false
+// null => in JS null it seld is a stand alone value, null is representation of empty value, empty value doesnt means let name = "" ; its means its a 
+//         empty string. let temp = null .suppose you are get temp from a api but temp is available due to some issue so in this case you can't 
+//         provide zero because zero it self a number, so null is prefered in this case
+// undefined => undefined is when you have not assign any value. let name; means a variable is declared but its value is not defined yet
+// symbol => its used for uniqueness
+
+// objects
+
+console.log(typeof "hitesh")
+console.log(typeof age)
+console.log(typeof undefined)  // type of undefined will be undefined, it means it a special type
+console.log(typeof null) // when you check type of null result will be object
