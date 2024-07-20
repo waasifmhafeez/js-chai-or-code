1e4c2124f758fe07c32c8e48302fd3da4b3731da
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-20 19:10:45 +0000

    completed strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -33,11 +33,11 @@ console.log(gameName.length);
 // console.log(gameName.indexof('t'));     // it will provide you the index of first accurance of the giving character, here its 2
 
 
-// const subString = gameName.substring(0,4);      // it will create a subString out of the orignal string. from index 0 to before index 4. 4 will not be included output: hite
+const subString = gameName.substring(0,4);      // it will create a subString out of the orignal string. from index 0 to before index 4. 4 will not be included output: hite
 // you cant use -ve value in substring method as parameter
 // console.log(subString);
 
-// const anotherStr = gameName.slice(-8,4)     
+const anotherStr = gameName.slice(-8,4)     
 
 // slice will give another from previous one, first index is start index, you can also put -ve value, incase of -ve it start counting from last index as -1 and all the back, in our case -8 
 // second index is end index, but it will not be included, it will give you character before 4. you can also use -ve values slice(1,-4) => slice(1, gameName.length -1) => slice(1,5)
@@ -46,5 +46,16 @@ console.log(gameName.length);
 
 const strOne = "   hitesh   ";
 console.log(strOne.trim());     // trim method trims the stating and ending spaces, which we dont want to store in out DBs 
+
+const url = "https://hitesh.com/hitesh%20choudary";  // sometime when url have some spaces like "https://wasif hafeez" browser will automatically change it to "https://wasif%20hafeez" so we dont want it to tackle it we will use replace method
+
+console.log(url.replace("%20",'-'));    // here first argument is what you wanna replace and second is with what you wanna replace, we replace %20 with -
+
+console.log(url.includes("hitesh"));  // include will tell if particular string or letter is present in our string, it will return true or false
+
+const strTwo = "hitesh-good-teacher";   //if you want to split this string into array, where ever "-" split output:['hitesh','good',teacher']
+
+console.log(strTwo.split("-"))
+
   
 
