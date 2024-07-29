64ac0fc02efe4dc11bc32a15e7cb716b77d63974
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-29 12:03:55 +0000

    completed 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -31,8 +31,8 @@ const JsUser = {
 // TO ADD SYMBOL IN OBJECT
 
 const mySymbol = Symbol("hello");   // to add symbol into a object, first you have to declare it
-console.log(mySymbol);
-console.log(`the type of mySymbol is => ${typeof mySymbol} `);
+// console.log(mySymbol);
+// console.log(`the type of mySymbol is => ${typeof mySymbol} `);
 
 const mySym2 = Symbol('hi')
 
@@ -42,7 +42,35 @@ const obj2 = {
     mySym2: "hehe"          // if use Symbol like this, then it will not be treated as symbol, it'll be a string
 }
 
-console.log(obj2[mySymbol]);
-console.log(obj2);              // even in object mySymbol is represented as symbol
+// console.log(obj2[mySymbol]);
+// console.log(obj2);              // even in object mySymbol is represented as symbol
 
-console.log(`the type of mySym2 is => ${typeof obj2.mySym2} and value is => ${obj2.mySym2}`);
+// console.log(`the type of mySym2 is => ${typeof obj2.mySym2} and value is => ${obj2.mySym2}`);
+
+// if you want to lock a object, so no one chanege it. use Object.freeze()
+
+JsUser.email = "hitesh@chatgpt.com";
+// Object.freeze(JsUser)       // Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+
+JsUser.email = "hitesh@microsoft.com";  // tried to modify email
+// console.log(JsUser);    // email didnt change bcs of Object.freeze()
+
+// Now lets add Functions to object - so JS functions are treated as typeone citizen, means there will be no discremination, you can also treat functions as variables
+
+JsUser.greeting = function(){
+    console.log('greetings from chai or code');
+}
+
+JsUser.greeting2 = function() {
+    console.log(`hello from greet 2 and ${this.name}`);
+}
+
+console.log(JsUser.greeting);   // if you write function name it will return you code of function or reference
+
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
+
+// INFO : in greeting & greetingTwo functions you  log messages to the console but don't return anything, so they implicitly return undefined as an output after every console. But when we use return keyword in functions instead of console.log it will not output 'undefined'.
+
+// sir the undefined value that is being printed on the screen is bcoz u r printing the return of the function which is not defined 
+// console.log(JsUser.greeting()) is wrong instead use JsUser.greeting() for calling the function
