2d50e1360e79c3410bfa738bdb2d4b2f8492ab30
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-31 15:20:45 +0000

    basics of var and let usage added in 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,24 @@
+// scope is {}
+// block scope {}
+
+// VAR
+var c = 300
+{
 let a = 10
 const b = 20
 var c = 30
+}
+// c=20        // if you declare a variable without any keyword it'll be "var"
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);  //undefined
+// console.log(b);  //undefined
+// console.log(c);   // if you use "c" out of the scope you can still access it, but you cant acccess the "a" and "b", they will be undefined
+
+// LET
+let aLet = 300
+{
+    let aLet = 400  // inside block "aLet" will be 400 and outside block it'll be 300
+    console.log("Inner =>",aLet);
+}
+console.log(aLet);
+
