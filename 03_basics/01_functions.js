8c998badb5e488926f7a58e11a821498230be78d
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-31 13:16:59 +0000

    01_functions is complete

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,8 +19,8 @@ function printMyName() {
 
 // now just you need to call the function as many times you want
 
-printMyName;    // its reference to the function
-printMyName();  // its calling the function
+// printMyName;    // its reference to the function
+// printMyName();  // its calling the function
 
 function addTwoNumbers(num1, num2) {
     console.log(num1 + num2);
@@ -31,8 +31,8 @@ function addTwoNumbers(num1, num2) {
 
 // you can also assign functions to other variables
 
-const result = addTwoNumbers(3,2);  // but your function isnt returning any thing
-console.log(result);        // it'll show undefined bcs function is not returning anything
+// const result = addTwoNumbers(3,2);  // but your function isnt returning any thing
+// console.log(result);        // it'll show undefined bcs function is not returning anything
 
 function addTwoNums(num1, num2){        // at function definition we have parameters
     // const result = num1 + num2;     // there are two ways (1) we can assign sum to a new variable and then return that
@@ -44,6 +44,54 @@ function addTwoNums(num1, num2){        // at function definition we have parame
 const sum = addTwoNums(3,2);    // now our function is returning result || at function call we have arguments
 // console.log(sum);
 
-function loginUserMessage(username){
-    return `${username} just logged in..`
+// function loginUserMessage(username){
+//     return `${username} just logged in..`
+// }
+// console.log(loginUserMessage("Hitesh"));
+// console.log(loginUserMessage());    // in case if you dont provide any parameters then it be undefined result=> undefined just logged in..
+
+// If user didnt provide argument then here we handled it..we showed a msg instead of showing undefined
+
+function loginUserMsg(username = 'sam'){   // agr hum chahte hein, kay username ki value kabhi undefined na ho. to PREDEFINED parameters bhi rakh sukhte hein
+    if(username === undefined){                     // you can also change condition to (!username)-- if username == undefined it will be as false- so ! false == true
+        console.log('please enter a username.');
+        return  ;
+    }
+    return `${username} is logged in`
 }
+const msg = loginUserMsg()
+// console.log(msg);
+
+// REST multiple parameter
+// agr humara function ak hi parameter accept kr raha hy magr user multiple parameter dedeta h. to os kay liye hum REST PARAMETERS ka use karein gy
+
+// function calculateCartPrice(num){    // in case of one single parameter it will work fine
+// function calculateCartPrice(...num){       // but if use give multiple params we can Rest then, rest operater put all the params into an array result=>[4,3,2,1]
+function calculateCartPrice(val1, val2, ...num){    // here it will assign 1st param to val1, 2nd param to val2, and remaing all will put in num array
+    return `val1 => ${val1}, val2 => ${val2} and all remaining num array => ${num} `
+}
+// console.log(calculateCartPrice(4,3,2,1,0))
+
+// passing Objects as arguments to a function
+
+const user = {
+    username: "Hitesh",
+    price: 999,
+}
+function handleObject (userObject) {
+    console.log(`Username is ${userObject.username} and price is ${userObject.price}`)
+}
+// handleObject(user);
+// we can also provide object directly as argument at function call
+
+// handleObject({username: "Hitesh Chaudary",price:199})
+ 
+// now passing array as Argument to Functions
+
+const newArr = [200, 300, 450, 530];
+function return2ndValue(getArray){
+    return getArray[1];
+}
+// console.log(return2ndValue(newArr));
+// or you can pass directly the array as arguments
+console.log(return2ndValue([200,450,300,600]));
