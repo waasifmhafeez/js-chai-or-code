193e60a445d293deb6be987ccef259c059967bbe
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-07-31 18:46:19 +0000

    scopes completed

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,7 +18,40 @@ var c = 30
 let aLet = 300
 {
     let aLet = 400  // inside block "aLet" will be 400 and outside block it'll be 300
-    console.log("Inner =>",aLet);
+    // console.log("Inner =>",aLet);
 }
-console.log(aLet);
+// console.log(aLet);
 
+function one (){
+    const username = "Hitesh";
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);   // "webiste" scope is inside function two, it cant be accessed outside of it
+    two();
+}
+one();      // ap is ko ayse yaad rukh sukte hein, kay chhotay bachhay baraon sy icecream mang sukte hein, but baray chhoton sy ni mang sukte
+
+// another example with the help of if-else
+if(true){
+    const username = "hitesh chaudary"
+    if(username === "hitesh chaudary"){
+        const website = "youtube"
+        console.log(username + " " + website);
+    }
+    // console.log(website);    // error: website cant be accessed outside of scope
+}
+// console.log(username);      // error: username cant be accessed outside of its scope
+
+// ******** Interesting ***********
+
+console.log(addOne(5))          // here if we call function before definition then there is no problem
+function addOne (num){
+    return num + 1;
+}
+addTwo(6)                         // if we call this function that we have assigned to const. there will be error : cant accedd "addTwo" before initialization
+const addTwo = function (num) {
+    return num + 2;
+}
+// addTwo(6)
