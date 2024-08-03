df432c0b66f0714357573e9408518ab3e90ca8de
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-08-03 18:44:00 +0000

    working on switch.js

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -8,3 +8,20 @@
 //     default:
 //         break;
 // }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Month is january.");
+        break;
+    case 2:
+        console.log("Month is febuary.");
+        break;
+    case 3:
+        console.log("Month is march.");
+        break;
+
+    default:
+        console.log("no case matched");
+        break;
+}
