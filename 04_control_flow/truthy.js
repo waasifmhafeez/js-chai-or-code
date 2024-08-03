25e799ac6d35eb5bc5f05ea95811fe2f272d29e9
waasifmhafeez <100529010+waasifmhafeez@users.noreply.github.com>
2024-08-03 18:53:47 +0000

    working on truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+const userEmail = "hitesh@ai"
+if(userEmail){
+    console.log("got user's email")
+}else{
+    console.log("dont have users email")
+}
+
+// Truthy & Falsy Values
+
+// Falsy values
+// false, 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+//Truthy values
+"0" , 'false', " ", [] , { } , function(){}
